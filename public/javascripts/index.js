7efd4aaa7fe13bca5b92420c2c4be0f618be2f21
warlyware <danward@gmail.com>
2015-08-24 17:50:03 -0400

    Add angular, materialize

---
+++ public/javascripts/index.js
@@ -0,0 +1,4 @@
+'use strict()';
+angular.module('GithubCardApp').controller('HomeCtrl', function($scope) {
+  console.log('HomeCtrl');
+});
---
+++ public/javascripts/main.js
@@ -0,0 +1,15 @@
+'use strict()';
+
+angular.module('GithubCardApp', ['ui.router'])
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {url: '/', templateUrl: '/templates/home.html', controller: "HomeCtrl"});
+  }).run(function(){
+    console.log('GithubCards Online');
+  })
+  .constant('urls',{
+    'apiUrl': ''
+  });
