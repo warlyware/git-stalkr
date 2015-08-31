a7f55ca42a77cda1f2635834ae1a2ea91efc332e
warlyware <danward@gmail.com>
2015-08-31 19:02:53 -0400

    Fix callback urls for deployment

--- config/auth.js
+++ config/auth.js
@@ -2,16 +2,16 @@ module.exports = {
   'googleAuth': {
     'clientID': process.env.GOOGLE_ID,
     'clientSecret': process.env.GOOGLE_SECRET,
-    'callbackURL': 'http://localhost:3000/auth/google/callback'
+    'callbackURL': process.env.SERVER + '/auth/google/callback'
   },
   'twitterAuth' : {
       'consumerKey'       : process.env.TWITTER_CONSUMER_ID,
       'consumerSecret'    : process.env.TWITTER_CONSUMER_SECRET,
-      'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+      'callbackURL'       : process.env.SERVER + '/auth/twitter/callback'
   },
   'githubAuth': {
     'clientID': process.env.GITHUB_CLIENT_ID,
     'clientSecret': process.env.GITHUB_CLIENT_SECRET,
-    'callbackURL': "http://localhost:3000/auth/github/callback"
+    'callbackURL': process.env.SERVER + '/auth/github/callback'
   }
 };
